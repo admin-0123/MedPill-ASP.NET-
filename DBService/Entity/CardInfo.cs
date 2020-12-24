5004069a6073c8664e19dd616572434f67db667b
AdrianMartin <am.dev.8080@gmail.com>
2020-12-24 19:57:15 +0800

    Added basic CardInfo properties

---
+++ DBService/Entity/CardInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBService.Entity
+{
+    public class CardInfo
+    {
+        //Properties of CardInfo
+        public string CardID { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public DateTime CardExpiry { get; set; }
+        public string CVVNumber { get; set; }
+
+        //Empty Constructor
+        public CardInfo()
+        {
+
+        }
+
+        //Constructor with parameters to initialise all properties
+        public CardInfo(string cardID, string cardName, string cardNumber, DateTime cardExpiry, string cvvNumber)
+        {
+            CardID = cardID;
+            CardName = cardName;
+            CardNumber = cardNumber;
+            CardExpiry = cardExpiry;
+            CVVNumber = cvvNumber;
+        }
+        public int Insert()
+        {
+            return 0;
+        }
+        //public CardInfo SelectByCardID(string cardID)
+        //{
+        //    return CardInfo;
+        //}
+
+        //public List<CardInfo> SelectAll()
+        //{
+
+        //}
+    }
+}
