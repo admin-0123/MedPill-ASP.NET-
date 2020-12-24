0290e39184ddf7344449ae7d4df51045f23e4e54
AdrianMartin <am.dev.8080@gmail.com>
2020-12-25 00:10:44 +0800

    Added more entity class

---
+++ DBService/Entity/Receipt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBService.Entity
+{
+    public class Receipt
+    {
+        public string ReceiptID { get; set; }
+        public DateTime DateSale { get; set; }
+        public double TotalSum { get; set; }
+        public Receipt()
+        {
+
+        }
+
+        public Receipt(string receiptID, DateTime dateSale, double totalSum)
+        {
+            ReceiptID = receiptID;
+            DateSale = dateSale;
+            TotalSum = totalSum;
+        }
+        public int Insert()
+        {
+            return 0;
+        }
+        //public Receipt SelectByReceiptID(string cardID)
+        //{
+        //    return Receipt;
+        //}
+
+        //public List<Receipt> SelectAll()
+        //{
+
+        //}
+    }
+}
---
+++ DBService/Entity/ReceiptItems.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBService.Entity
+{
+    public class ReceiptItems
+    {
+        public string ItemID { get; set; }
+        public double ItemCost { get; set; }
+        public int ItemQuantity { get; set; }
+        public string ItemDescription { get; set; }
+        public double Subtotal { get; set; }
+        public ReceiptItems()
+        {
+
+        }
+        public ReceiptItems(string itemID,double itemCost, int itemQuantity, string itemDescription, double subtotal)
+        {
+            ItemID = itemID;
+            ItemCost = itemCost;
+            ItemQuantity = itemQuantity;
+            ItemDescription = itemDescription;
+            Subtotal = subtotal;
+        }
+        public int Insert()
+        {
+            return 0;
+        }
+        //public ReceiptItems SelectByReceiptItemID(string cardID)
+        //{
+        //    return ReceiptItems;
+        //}
+
+        //public List<ReceiptItems> SelectAll()
+        //{
+
+        //}
+    }
+}
