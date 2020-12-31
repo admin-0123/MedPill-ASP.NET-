a4f6622a5d17fcbd16bda2cd81553f8823fc6369
AdrianMartin <am.dev.8080@gmail.com>
2020-12-31 23:48:55 +0800

    Added Create and Retrieve Functions for CardInfo Class

--- DBService/Entity/CardInfo.cs
+++ DBService/Entity/CardInfo.cs
@@ -42,13 +42,63 @@ namespace DBService.Entity
             string DBConnect = ConfigurationManager.ConnectionStrings["EDP_DB"].ConnectionString;
             SqlConnection myConn = new SqlConnection(DBConnect);
 
-            //will continue adding more here
-            return 0;
+            //Step 2 - Create SQL Statement
+            string sqlStatement = "INSERT INTO CardInfo (CardID, CardName, CardNumber, CardExpiry, CVVNumber, StillValid)"
+                + "VALUES (@paraCardID, @paraCardName, @paraCardNumber, @paraCardExpiry, @paraCVVNumber, @paraStillValid)";
+
+            SqlCommand sqlCmd = new SqlCommand(sqlStatement, myConn);
+
+            //Step 3 - Add info to each parameterised variables
+            sqlCmd.Parameters.AddWithValue("paraCardID", CardID);
+            sqlCmd.Parameters.AddWithValue("paraCardName", CardName);
+            sqlCmd.Parameters.AddWithValue("paraCardNumber",CardNumber);
+            sqlCmd.Parameters.AddWithValue("paraCardExpiry",CardExpiry);
+            sqlCmd.Parameters.AddWithValue("paraCVVNumber",CVVNumber);
+            sqlCmd.Parameters.AddWithValue("paraStillValid",StillValid);
+
+            //Step 4 - Open Connection to database
+            myConn.Open();
+            int result = sqlCmd.ExecuteNonQuery();
+
+            //Step 5 - Close Connection to database
+            myConn.Close();
+
+            return result;
         }
-        //public CardInfo SelectByCardID(string cardID)
-        //{
-        //    return CardInfo;
-        //}
+        public CardInfo SelectByCardID(string cardID)
+        {
+            //Step 1 -  Define a connection to the database by getting
+            //          the connection string from App.config
+            string DBConnect = ConfigurationManager.ConnectionStrings["EDP_DB"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            //Step 2 - Create DataAdapter to retrieve data from database table
+            string sqlStatement = "SELECT * FROM CardInfo WHERE CardID = @paraCardID";
+            SqlDataAdapter da = new SqlDataAdapter(sqlStatement, myConn);
+            da.SelectCommand.Parameters.AddWithValue("@paraCardID", cardID);
+
+            //Step 3 - Create a dataset to store data to be retrieved
+            DataSet ds = new DataSet();
+
+            //Step 4 - Use DataAdapter to fill dataset with retrieved data
+            da.Fill(ds);
+
+            //Step 5 - Read data from dataset
+            CardInfo cif = null;
+            int rec_cnt = ds.Tables[0].Rows.Count;
+            if(rec_cnt == 1)
+            {
+                DataRow row = ds.Tables[0].Rows[0]; //Returns one record
+                //string cardID = row["CardID"].ToString();
+                string cardName = row["CardName"].ToString();
+                string cardNumber = row["CardNumber"].ToString();
+                DateTime cardExpiry = Convert.ToDateTime(row["CardExpiry"].ToString());
+                string cvvNumber = row["CVVNumber"].ToString();
+                bool stillValid = Convert.ToBoolean(row["CardName"].ToString());
+                cif = new CardInfo(cardID, cardName, cardNumber, cardExpiry, cvvNumber);
+            }
+            return cif;
+        }
 
         //public List<CardInfo> SelectAll()
         //{
