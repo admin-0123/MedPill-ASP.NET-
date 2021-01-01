77a4a0c268507867a99f4bbcd91988c1245fda6d
AdrianMartin <am.dev.8080@gmail.com>
2021-01-01 15:05:13 +0800

    small fix to login related cs pages

--- EDP_Clinic/AdminPage.aspx.cs
+++ EDP_Clinic/AdminPage.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace EDP_Clinic
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class AdminPage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
