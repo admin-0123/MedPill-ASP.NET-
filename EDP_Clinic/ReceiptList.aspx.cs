ef549241612a228ddb37b5d99d9689eeb916c5f0
AdrianMartin <am.dev.8080@gmail.com>
2021-01-23 19:03:41 +0800

    Added pagination for receiptlist page and minor modification to footer

--- EDP_Clinic/ReceiptList.aspx.cs
+++ EDP_Clinic/ReceiptList.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EDP_Clinic.EDP_DBReference;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,30 @@ namespace EDP_Clinic
     public partial class Receipt : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            //getReceiptList();
+            
+        }
+
+        protected void getReceiptList()
+        {
+            List<CardInfo> cifList = new List<CardInfo>();
+            Service1Client client = new Service1Client();
+            cifList = client.GetAllCards().ToList<CardInfo>();
+
+            receiptListView.DataSource = cifList;
+            receiptListView.Visible = true;
+            //receiptListView.DataBind();
+        }
+
+        //Rename datapager 1 to receiptListPager
+        protected void DataPager1_PreRender(object sender, EventArgs e)
+        {
+            getReceiptList();
+            receiptListView.DataBind();
+        }
+
+        protected void backBtn_Click(object sender, EventArgs e)
         {
 
         }
