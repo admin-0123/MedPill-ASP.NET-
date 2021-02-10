e40d5855691f50f99108b0d782513b6ce9316cd9
AdrianMartin <am.dev.8080@gmail.com>
2021-02-11 00:49:31 +0800

    .

---
+++ DBService/Entity/displayPatient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBService.Entity
+{
+    class displayPatient
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        public string Verified { get; set; }
+        public string IsPatient { get; set; }
+
+    }
+}
