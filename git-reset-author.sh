67986803d54485b8fa06af203600f3d422befd85
admin-0123 <am.dev.8080@gmail.com>
2024-02-05 19:39:38 -0500

    Updated readme

--- git-reset-author.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Credits: http://stackoverflow.com/a/750191
-
-git filter-branch -f --env-filter "GIT_AUTHOR_NAME='AdrianMartin' GIT_AUTHOR_EMAIL='am.dev.8080@gmail.com' GIT_COMMITTER_NAME='adrianmartin' GIT_COMMITTER_EMAIL='am.dev.8080@gmail.com'" HEAD
